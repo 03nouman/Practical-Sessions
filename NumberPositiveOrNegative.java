a0d8186491a8eb175b1c687ada7b4455a8939c09
03nouman <107634861+03nouman@users.noreply.github.com>
2022-07-14 13:00:03 +0530

    Add files via upload

---
+++ NumberPositiveOrNegative.java
@@ -0,0 +1,20 @@
+class NumberPositiveOrNegative
+{
+ public static void main(String[] a)
+{
+ int number = 10;
+ 
+if(number>0)
+{
+  System.out.println("The Number is Positive");
+}
+else if(number<0)
+{
+ System.out.println("The Number is Nagative");
+}
+else
+{
+ System.out.println("The Number is Not positive nor negative");
+}
+}
+}
